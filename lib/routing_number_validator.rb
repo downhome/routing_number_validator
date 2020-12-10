819f0a953bd698addfb24aa7c9d2e2e5d21814ff
Maher Hawash <gmhawash@gmail.com>
2020-12-10 12:34:43 -0800

    Refactor

--- lib/routing_number_validator.rb
+++ lib/routing_number_validator.rb
@@ -1,33 +1,17 @@
 class RoutingNumberValidator < ActiveModel::EachValidator
-
   def validate_each(record, attribute, value)
-    unless value && value.to_s.size == 9
+    if value.blank? || value.to_s.size != 9
       record.errors[attribute] << (options[:message] || "must be 9 digits long")
-    end
-
-    unless valid_routing_number?(value)
+    elsif !valid_routing_number?(value)
       record.errors[attribute] << (options[:message] || "prefix must be 01-12 or 21-32")
     end
   end
 
   private
 
-  # source: https://github.com/Shopify/active_merchant/blob/master/lib/active_merchant/billing/check.rb#L53
+  # source: https://en.wikipedia.org/wiki/ABA_routing_transit_number#Internal_checksums
   def valid_routing_number?(routing_number)
-    digits = routing_number.to_s.split('').map(&:to_i).select{|d| (0..9).include?(d)}
-    case digits.size
-    when 9
-      checksum = ((3 * (digits[0] + digits[3] + digits[6])) +
-                  (7 * (digits[1] + digits[4] + digits[7])) +
-                       (digits[2] + digits[5] + digits[8])) % 10
-      case checksum
-      when 0
-        true
-      else
-        false
-      end
-    else
-      false
-    end
+    digits = routing_number.to_s.gsub(/[^\d]/, '').split('').map(&:to_i)
+    0 === (digits.each_slice(3).map{|a,b,c| a*3 + b*7 + c}.sum) % 10
   end
 end
