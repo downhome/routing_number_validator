ab25215201c415f45c18973eaddd90b7eaa3239b
gmhawash <gmhawash@gmail.com>
2023-11-09 15:33:17 -0800

    Check length and checksum

--- lib/routing_number_validator.rb
+++ lib/routing_number_validator.rb
@@ -4,11 +4,10 @@ require_relative 'valid_routing_number'
 
 class RoutingNumberValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    value = value.to_s.gsub(/[^\d]/, '')
-
-    if value.blank? || value.to_s.size != 9
+    case ValidRoutingNumber.call(value)
+    when :invalid_length
       record.errors[attribute] << (options[:message] || "routing number must be 9 digits long")
-    elsif !ValidRoutingNumber.call(value)
+    when :invalid_checksum
       record.errors[attribute] << (options[:message] || "invalid routing number. Failed checksum.")
     end
   end
--- lib/valid_routing_number.rb
+++ lib/valid_routing_number.rb
@@ -3,7 +3,30 @@
 # source: https://en.wikipedia.org/wiki/ABA_routing_transit_number#Internal_checksums
 class ValidRoutingNumber
   def self.call(routing_number)
-    routing_number = "%09d" % routing_number.to_s.gsub(/[^\d]/, '')
+    new(routing_number).call
+
+  end
+
+  def initialize(routing_number)
+    @routing_number = routing_number.to_s.gsub(/[^\d]/, '')
+  end
+
+  def call
+    if invalid_length?
+      :invalid_length
+    elsif invalid_checksum?
+      :invalid_checksum
+    end
+  end
+
+  private
+  attr_reader :routing_number
+
+  def invalid_length?
+    routing_number.size != 9
+  end
+
+  def invalid_checksum?
     digits = routing_number.split('').map(&:to_i)
 
     checksum = digits
@@ -11,6 +34,7 @@ class ValidRoutingNumber
       .map{|a,b,c| a * 3 + b * 7 + c}
       .sum
 
-    0 === checksum % 10
+    checksum % 10 != 0
   end
+
 end
