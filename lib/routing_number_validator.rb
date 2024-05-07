2143339d0d47ffc87b5ae62360a8c48ff5cba623
Saher Haijawi <saherhaijawi@gmail.com>
2024-05-07 14:35:51 +0300

    DC-10324: fix errors issue

--- lib/routing_number_validator.rb
+++ lib/routing_number_validator.rb
@@ -6,9 +6,9 @@ class RoutingNumberValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     case ValidRoutingNumber.call(value)
     when :invalid_length
-      record.errors[attribute] << (options[:message] || "routing number must be 9 digits long")
+      record.errors.add(attribute, (options[:message] || "routing number must be 9 digits long"))
     when :invalid_checksum
-      record.errors[attribute] << (options[:message] || "invalid routing number. Failed checksum.")
+      record.errors.add(attribute, (options[:message] || "invalid routing number. Failed checksum."))
     end
   end
 end
